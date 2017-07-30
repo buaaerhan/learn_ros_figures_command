f86f5ae814f23807ff01717e5a43d592d649c632
robin.han <hanjf_robin@163.com>
2017-07-30 03:22:53 -0700

    添加节点相应的注释

--- scripts/commander.py
+++ scripts/commander.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python
 
+# 实际上每个node就是一个可执行文件，这里我们使用了rospy客户端库，即ROS的Python客户端，因此
+# 我们需要编写Python程序作为ROS的node
+
+# 首先加载rospy客户端库，用以创建节点
+# 然后从learn_ros_figures_command/msg文件夹中加载Figures类型的消息文件，注意导入方式
 import rospy
 from learn_ros_figures_command.msg import Figures
 
+# 定义节点函数
 def commander():
-    pub = rospy.Publisher('draw_figure',Figures,queue_size=10)
-    rospy.init_node('figures_commander',anonymous=True)
-    rate = rospy.Rate(10)
+    pub = rospy.Publisher('draw_figure',Figures,queue_size=10)  # 定义节点为一个消息发送者publisher，
+    rospy.init_node('figures_commander',anonymous=True)         # 发送Figures类型的消息到draw_figure这个话题上，频率为
+    rate = rospy.Rate(10)                                       # 节点名称默认为figures_commander,支持系统自动分配名字
+    
+    # 在上面代码中需要注意的是话题draw_figure的定义，是没有额外
+    # 代码的，只是一个输入字符串而已
 
     while not rospy.is_shutdown():
-        figure = raw_input('Input figure command: ')
-        pub.publish(figure)
+        figure = raw_input('Input figure command: ')            # 输入指令
+        pub.publish(figure)                                     # 将消息对象figure发送出去(发送到之前定义的topic上去)
         rate.sleep()
 
 if __name__ == '__main__':
--- scripts/drawer.py
+++ scripts/drawer.py
@@ -1,26 +1,36 @@
 #!/usr/bin/env python
 
+# 本节点注册到draw_figure话题，从其接收消息，并执行相应指令
+# 注意需要用到matplotlib和numpy支持包，python版本为2.7
+
 import matplotlib.pyplot as plt
 import numpy as np
 import rospy
 from learn_ros_figures_command.msg import Figures
 
+# 定义画圆函数，输入参量为圆半径
 def draw_circle(radius):
     x = np.arange(0,2*np.pi,0.01)
     plt.plot(radius*np.cos(x),radius*np.sin(x))
     plt.show()
 
+# 定义画图函数，输入参量为接收到的消息，可根据不同的指令画出不同的图形
+# 目前仅支持画圆，作为演示所用
 def draw(figure):
-    if(figure.figures=='circle'):
-        draw_circle(5)
+    if(figure.figures=='circle'):       # 注意，传入参量为figure，是一个Figures类型的消息对象，可以调用其中的
+        draw_circle(5)                  # 属性数据figures，以查看输入的具体数据
 
     else:
         print 'Please input "circle"'
 
 def drawer():
     rospy.init_node('figures_drawer',anonymous=True)
-    rospy.Subscriber('draw_figure',Figures,draw)
-    rospy.spin()
+    rospy.Subscriber('draw_figure',Figures,draw)        # 此处与定义publisher时的情形类似，话题仍是以字符串的
+    rospy.spin()                                        # 的形式传入函数，上面的消息类型为Figures
+
+# 上面的代码与定义publisher时的情形类似，话题仍是以字符串的形式传入函数
+# 话题上承载的消息类型为Figures，在接收到Figures类型的消息之后，将消息
+# 作为参数传入到回调函数draw(figure)中去，画出相应图形
 
 if __name__=='__main__':
     drawer()
